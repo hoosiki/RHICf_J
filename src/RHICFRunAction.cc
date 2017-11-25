c24a10282c3a7e285678337bdd78bdd285e1d0cb
hoosiki <parkjunsang0405@gmail.com>
2017-11-25 03:38:50 -0500

    complete primarygenerator

--- src/RHICFRunAction.cc
+++ src/RHICFRunAction.cc
@@ -25,7 +25,7 @@ RHICFRunAction::RHICFRunAction(RHICFEventAction* eventAction): G4UserRunAction()
 
     FileManager* fFileManager = FileManager::GetInstance();
     fFileManager->PrepareSavingDirectory();
-    //Junsang****fFileManager->SetFileName("test");
+    fFileManager->SetFileName("LEAKGESTUDYNEUTRONTL");
     G4AnalysisManager::Instance()->SetNtupleMerging(true);
     G4AnalysisManager::Instance()->SetVerboseLevel(0);
     G4AnalysisManager::Instance()->SetFileName(fFileManager->GetPathFortmp()+"/"+fFileManager->GetFileName()+".root");
